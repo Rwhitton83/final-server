b67a438ccf4022f45f05851f9ada1d161a2dcaf8
Rwhitton83 <whittor@sunypoly.edu>
2021-05-10 15:01:37 -0400

    first

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express")
 const app = express()
 const mysql = require("mysql")
 const cors = require('cors')
+const bodyParser = require("body-parser")
 
 app.use(express.json());
 app.use(cors({
